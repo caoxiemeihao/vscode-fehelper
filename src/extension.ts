22be4e5279fd720c82452b58b5344d820f400b13
草鞋没号 <308487730@qq.com>
2022-02-10 17:28:11 +0800

    add plugin Node Dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { NodeDependenciesProvider } from './NodeDependenciesProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+	vscode.window.registerTreeDataProvider(
+		'nodeDependencies',
+		// @ts-ignore
+		new NodeDependenciesProvider(vscode.workspace.rootPath)
+	);
+	vscode.window.createTreeView('nodeDependencies', {
+		// @ts-ignore
+		treeDataProvider: new NodeDependenciesProvider(vscode.workspace.rootPath)
+	});
+	// @ts-ignore
+	const nodeDependenciesProvider = new NodeDependenciesProvider(vscode.workspace.rootPath);
+  vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
+  vscode.commands.registerCommand('nodeDependencies.refreshEntry', () =>
+    nodeDependenciesProvider.refresh()
+  );
+
+	// ---------------------------------------------------------------------------
+
+
+
+	// ---------------------------------------------------------------------------
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-fe-helper" is now active!');
@@ -23,4 +45,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
